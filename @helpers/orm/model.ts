3c53408e383cd07e8e460c2fd3018f3cee26d5bd
juno <shards552@gmail.com>
2023-05-15 00:07:48 +0800

    express setup

---
+++ @helpers/orm/model.ts
@@ -0,0 +1,5 @@
+export default class Model {
+    protected table: string;
+    protected primaryKey: BigInt;
+    protected fillable: Array<string>;
+}
---
+++ setup.local.sh
@@ -0,0 +1,5 @@
+npm init -y
+npm i express dotenv cors helmet nodemailer
+npm i -D typescript
+npm i -D @types/node @types/express @types/dotenv @types/cors @types/helmet @types/nodemailer
+npm i -D ts-node-dev
